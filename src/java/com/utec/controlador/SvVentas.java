2c257d3e1f82bf91c9a6068aaaa37674cd932d5b
CASTRO CONTRERAS BRANDON ALEXANDER <brandoncastro200314@gmail.com>
2023-09-26 09:24:25 -0600

    diseno del modificar ventas

--- src/java/com/utec/controlador/SvVentas.java
+++ src/java/com/utec/controlador/SvVentas.java
@@ -49,7 +49,9 @@ public class SvVentas extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String accion = request.getParameter("accion");
-        
+        //lista de clientes para que el usuario seleccione el cliente de la venta
+        List<Cliente> cliente = new ClienteDao().listar();
+        request.setAttribute("cliente",cliente);
         switch(accion){
             case "listar":
                  List<Ventas> clientes = new VentasDao().listar();
@@ -57,8 +59,6 @@ public class SvVentas extends HttpServlet {
                 request.getRequestDispatcher("ventas/listar.jsp").forward(request, response);
                 break;
             case "agregar":
-                List<Cliente> cliente = new ClienteDao().listar();
-                request.setAttribute("cliente",cliente);
                 request.getRequestDispatcher("ventas/agregar.jsp").forward(request,response);
                 break;
             case "modificar":
